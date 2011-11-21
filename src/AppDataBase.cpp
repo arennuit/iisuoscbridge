264a49eed81a0b78946b613bba9fa0caeb31a590
Antoine Rennuit <are@softkinetic.com>
2011-11-21 15:31:56 +0100

    Had forgotten to add those files to git...

---
+++ src/AppDataBase.cpp
@@ -0,0 +1,26 @@
+#include "AppDataBase.h"
+
+#include "MainForm.h"
+
+namespace SK
+{
+
+DEFINE_DATA_BASE(AppDataBase, DataBase)
+
+//////////////////////////////////////////////////////////////////////////
+void AppDataBase::setIsObservationOn(bool isObservationOn)
+{
+	DataBase::setIsObservationOn(isObservationOn);
+
+	m_mainForm->onIsObservationOnChanged(isObservationOn);
+}
+
+//////////////////////////////////////////////////////////////////////////
+void AppDataBase::setOscPacketSize(uint oscPacketSize)
+{
+	DataBase::setOscPacketSize(oscPacketSize);
+
+	m_mainForm->onOscPacketSizeChanged(m_oscPacketSize);
+}
+
+} // namespace SK.
---
+++ src/AppDataBase.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "DataBase/DataBase.h"
+
+namespace SK
+{
+
+class MainForm;
+
+//////////////////////////////////////////////////////////////////////////
+/// \brief This class adds the UI update service on data change.
+class AppDataBase : public DataBase
+{
+	DECLARE_DATA_BASE(AppDataBase)
+
+public:
+
+	/// \name Application data accessors redefinitions.
+	//@{
+	void setIsObservationOn(bool isObservationOn) SK_OVERRIDE;
+	void setOscPacketSize(uint oscPacketSize) SK_OVERRIDE;
+	//@}
+
+	/// \name Accessors.
+	//@{
+	void setMainForm(MainForm* mainForm) {m_mainForm = mainForm;}
+	//@}
+
+protected:
+	AppDataBase() : DataBase(), m_mainForm(0) {}
+	virtual ~AppDataBase() {}
+
+	MainForm* m_mainForm;
+};
+
+} // namespace SK.
